35fc903a26a4607075e1413867ed4dfd007d441a
HT <chenyuw2019@gmail.com>
2025-09-30 09:35:34 -0400

    Answered question 33

--- Responder.java
+++ Responder.java
@@ -1,4 +1,4 @@
-
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -8,20 +8,43 @@
  */
 public class Responder
 {
+    private HashMap<String, String> responses;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        responses = new HashMap<>();
+        fillResponsesMap();
+    }
+    
+    private void fillResponsesMap()
+    {
+        responses.put("slow", "Try turning off your computer.");
+        responses.put("crash", "You're gonna have to call a real tech support.");
+        responses.put("slow", "What's the error message?");
+        responses.put("slow", "Check whether your monitor is plugged in properly.");
+        responses.put("slow", "Unpair and pair the device again.");
+        responses.put("slow", "Turn if off and then back on, and check if airplane mode is turned off.");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String word)
     {
-        return "That sounds interesting. Tell me more...";
+        String response = responses.get(word.toLowerCase());
+        if(responses != null) {
+            return response;
+        } else {
+            return pickDefaultResponse();
+        }
+    }
+    
+    private String pickDefaultResponse()
+    {
+        return "Could you be more specific?";
     }
 }
 
